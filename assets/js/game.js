0edfcc64cbf09e945c19c816fdcd6d989fe0152e
Master Chief <kemar7@live.com>
2020-06-20 06:38:07 -0400

    add end game function

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,10 +72,51 @@ var fight = function(enemyName) {
         }
     }
 };
- 
-for(var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
-}
 
+
+
+// function to end game
+var endGame = function() {
+    // function to start a new game
+var startGame = function() {
+     // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+   
+    for(var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            
+            var pickedEnemyName = enemyNames[i];
+            
+            enemyHealth = 50;
+            
+            fight(pickedEnemyName);
+        } else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
+    }
+    
+    // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    endGame();
+};
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of "  + playerMoney + ".");
+    } else {
+        window.alert("You've lost your robot battle.");
+    }
+
+    // ask player if they'd like to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+      // restart the game
+    startGame();
+      } 
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+endGame();
